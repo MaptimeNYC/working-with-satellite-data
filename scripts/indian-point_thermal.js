e9c450392d8197121e4bed9fd1951b5f2e54c825
bfreeds <brandynfriedly@gmail.com>
2017-01-14 14:39:10 -0500

    use raw landsat8 instead of 8-day composite

--- scripts/indian-point_thermal.js
+++ scripts/indian-point_thermal.js
@@ -3,10 +3,18 @@
  */
 Map.addLayer(
   //  add collection of raw Landsat8 data
-  ee.ImageCollection('LANDSAT/LC8_L1T_8DAY_RAW');
+  ee.ImageCollection('LANDSAT/LC8_L1T');
 );
 
 /*
  *  2.  Center map on Indian Point
  */
 Map.setCenter(-73.954661, 41.269739, 15);
+
+/*
+ *  3.  Filter collection to useful data
+ */
+ const indianPoint = ee.Geometry.Point(-73.954661, 41.269739);
+
+ var start = ee.Date('')
+ var finish = ee.Date('')
