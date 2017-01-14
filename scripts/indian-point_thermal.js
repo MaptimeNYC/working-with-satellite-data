972b2edcf719a2dcaf12d68e9edadf2b1bf72509
bfreeds <brandynfriedly@gmail.com>
2017-01-14 14:18:25 -0500

    center map view on the coast of Indian Point

--- scripts/indian-point_thermal.js
+++ scripts/indian-point_thermal.js
@@ -5,3 +5,8 @@ Map.addLayer(
   //  add collection of raw Landsat8 data
   ee.ImageCollection('LANDSAT/LC8_L1T_8DAY_RAW');
 );
+
+/*
+ *  2.  Center map on Indian Point
+ */
+Map.setCenter(-73.954661, 41.269739, 15);
