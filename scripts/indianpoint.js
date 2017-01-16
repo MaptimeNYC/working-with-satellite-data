3829b633c7c140032ee787a3304bb8d8132a6211
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-16 13:36:41 -0500

    filter image collection to Indian Point

--- scripts/indianpoint.js
+++ scripts/indianpoint.js
@@ -3,3 +3,10 @@ var LC8_coll = ee.ImageCollection('LANDSAT/LC8_L1T');
 
 // Set coordinates for Indian Point
 var ip_point = ee.Geometry.Point(-73.9513992,41.267997);
+
+// Filter image collection to Indian Point
+var ip_coll = ee.ImageCollection('LANDSAT/LC8_L1T')
+  .filterBounds(ip_point);
+print(ip_coll);
+
+Map.addLayer(ip_coll);
