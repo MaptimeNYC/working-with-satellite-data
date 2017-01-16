b5d0f5ce60d7ae916739db7f99c9ae06677d7a31
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-16 13:29:47 -0500

    set coordinates for indian point

--- scripts/indianpoint.js
+++ scripts/indianpoint.js
@@ -1,5 +1,5 @@
-// Add Landsat8 collection
-
+// Add Landsat8 image collection
 var LC8_coll = ee.ImageCollection('LANDSAT/LC8_L1T');
 
-// Map.addLayer(LC8_coll);
+// Set coordinates for Indian Point
+var ip_point = ee.Geometry.Point(-73.9513992,41.267997);
