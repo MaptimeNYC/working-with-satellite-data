6e608b3a63c31b2c0c704228b6b3dfa76253092d
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-24 15:56:11 -0500

    add comments

--- scripts/indianpoint.js
+++ scripts/indianpoint.js
@@ -1,16 +1,20 @@
+// This script will take some LANDSAT 8 data, filter it down, and display it on a map
+
 // Add Landsat8 image collection
 var LC8_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA');
 
-// Set coordinates for Indian Point
+/*
+// One way to filter by location
+// Filter image collection to Indian Point using point
+// Results in multiple scenes, so see method #2 below
 var ip_point = ee.Geometry.Point(-73.9513992,41.267997);
-
-/*// Filter image collection to Indian Point using point
 var ip_image = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
   .filterBounds(ip_point);
 print(ip_image);
 */
 
 // Filter image collection to Indian Point using WRS Path/Row
+// Use NASA's Earth Explorer to find path/row for each LANDSAT scene
 var ip_image = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
   .filter(ee.Filter.eq('WRS_PATH', 13))
   .filter(ee.Filter.eq('WRS_ROW', 31));
@@ -22,8 +26,9 @@ print('Metadata properties: ', ip_properties);
 // Filter image collection to Spring 2016
 var spring2016 = ip_image.filterDate('2016-03-01', '2016-06-01');
 
-// Compute a median image and display
+// Compute the median value of each pixel based on filtered collection
+// Display RGB bands to visualize scene
 var medianSpring16 = spring2016.median();
-Map.setCenter(-73.9513992,41.267997,);
+Map.setCenter(-73.9513992,41.267997,9);
 Map.addLayer(medianSpring16,
   {bands: ['B4', 'B3', 'B2'], max: 0.3}, 'medianSpring16');
