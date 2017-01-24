acb19f663a0dad24530f86279389c7be113cd6f6
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-24 16:22:41 -0500

    visualize thermal band

--- scripts/ipthermal.js
+++ scripts/ipthermal.js
@@ -6,6 +6,13 @@ var ip_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
   .filter(ee.Filter.eq('WRS_PATH', 13))
   .filter(ee.Filter.eq('WRS_ROW', 31));
 
-  // Create buffer for analysis
-  var geom = ee.Geometry.Point(-73.9513992,41.267997)
-    .buffer(1000);
+// Filter image collection to Spring 2016
+  var spring2016 = ip_coll.filterDate('2016-03-01', '2016-06-01');
+
+// Compute the median value of each pixel based on filtered collection
+// Display thermal to visualize scene
+  var medianSpring16 = spring2016.median();
+
+  Map.setCenter(-73.9513992,41.267997,9);
+  Map.addLayer(medianSpring16,
+      {bands: ['B10'], min: 270, max: 310});
