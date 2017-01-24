85b854dd8c3f7e89a05530855b15bdb8e7617fb0
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-24 11:39:14 -0500

    filter by cloud cover

--- scripts/indianpoint.js
+++ scripts/indianpoint.js
@@ -4,17 +4,18 @@ var LC8_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA');
 // Set coordinates for Indian Point
 var ip_point = ee.Geometry.Point(-73.9513992,41.267997);
 
-/*
-// Filter image collection to Indian Point using point
-var ip_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
-  .filterBounds(ip_point);
-print(ip_coll);
-*/
 
+// Filter image collection to Indian Point using point
+var ip_image = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
+  .filterBounds(ip_point);
+print(ip_image);
+
+/*
 // Filter image collection to Indian Point using WRS Path/Row
 var ip_image = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
   .filter(ee.Filter.eq('WRS_PATH', 13))
   .filter(ee.Filter.eq('WRS_ROW', 31));
+*/
 
 // Filter image collection to Spring 2016
 var spring2016 = ip_image.filterDate('2016-03-01', '2016-06-01');
@@ -24,5 +25,9 @@ print(spring2016);
 var ip_properties = ip_image.propertyNames();
 print('Metadata properties: ', ip_properties);
 
-Map.addLayer(spring2016);
+// Filter image collection by cloud cover
+var filteredCollection = (spring2016)
+  .sort('CLOUD_COVER', true);
+
+Map.addLayer(filteredCollection);
 Map.setCenter(-73.9513992,41.267997);
