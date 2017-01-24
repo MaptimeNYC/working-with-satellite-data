6a8d4f00c7bdc2e3f50578a9a38893843dda18bd
Beryl Sinclair <beryl.sinclair@gmail.com>
2017-01-24 16:05:55 -0500

    add spherical buffer for analysis

--- scripts/ipthermal.js
+++ scripts/ipthermal.js
@@ -5,3 +5,7 @@ var LC8_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA');
 var ip_coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
   .filter(ee.Filter.eq('WRS_PATH', 13))
   .filter(ee.Filter.eq('WRS_ROW', 31));
+
+  // Create buffer for analysis
+  var geom = ee.Geometry.Point(-73.9513992,41.267997)
+    .buffer(1000);
